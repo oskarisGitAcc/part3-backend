df494a92eb7626c2ca0ee3ed18c06e771bd4a5cc
Oskari Pyykkönen <oskari.pyykkonen@gmail.com>
2024-01-23 17:19:00 +0200

    Part 3b production build

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 see https://github.com/oskarisGitAcc/fullStackWebDevelopment for frontend
 part as well as cleaner commit structure for part 3a. The whole part is
 put to single commit in the beginning of this repo.
+
+Commands:
+
+- fly deploy = Whenever you make changes to the application, you can take the new version to production with a command
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const cors = require('cors')
 
 morgan.token('postData', (req) => {
   return JSON.stringify(req.body);
 });
 
+app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'));
 app.use(express.json());
+app.use(express.static('dist'))
 
 let persons = [
   { 
@@ -95,7 +98,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 });
