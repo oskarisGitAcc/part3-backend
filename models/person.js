56321517e50c27c903aa9a3e68faa9a5d419585f
Oskari Pyykkönen <oskari.pyykkonen@gmail.com>
2024-01-30 16:09:01 +0200

    3.22: Lint configuration

--- models/person.js
+++ models/person.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-mongoose.connect(url).then(result => {
-  console.log('connected to MongoDB');
+mongoose.connect(url).then(() => {
+  console.log('connected to MongoDB')
 }).catch((error) => {
-  console.log('error connecting to mongoDB: ', error.message);
-});
+  console.log('error connecting to mongoDB: ', error.message)
+})
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -22,20 +22,20 @@ const personSchema = new mongoose.Schema({
     minLength: [9, 'Phone number must be at least 8 characters long'],
     validate: {
       validator: function(value) {
-        const phoneNumberForm = /\d{2,3}-\d{5,}/;
-        return phoneNumberForm.test(value);
+        const phoneNumberForm = /\d{2,3}-\d{5,}/
+        return phoneNumberForm.test(value)
       },
       message: props => `${props.value} is not a valid phone number!`
     }
   }
-});
+})
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   }
-});
+})
 
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model('Person', personSchema)
