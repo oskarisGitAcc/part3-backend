6dfdb022220483ffacaa7398910152370969d7ad
Oskari Pyykkönen <oskari.pyykkonen@gmail.com>
2024-01-30 14:57:37 +0200

    3.20*-3.21: Phonebook database, step8

--- models/person.js
+++ models/person.js
@@ -5,20 +5,28 @@ mongoose.set('strictQuery', false);
 const url = process.env.MONGODB_URI;
 
 mongoose.connect(url).then(result => {
-    console.log('connected to MongoDB');
-  }).catch((error) => {
-    console.log('error connecting to mongoDB: ', error.message);
-  });
+  console.log('connected to MongoDB');
+}).catch((error) => {
+  console.log('error connecting to mongoDB: ', error.message);
+});
 
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
-    minLength: 3,
-    required: true
+    required: true,
+    minLength: 3
   },
   number: {
     type: String,
-    required: true
+    required: true,
+    minLength: [9, 'Phone number must be at least 8 characters long'],
+    validate: {
+      validator: function(value) {
+        const phoneNumberForm = /\d{2,3}-\d{5,}/;
+        return phoneNumberForm.test(value);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
   }
 });
 
